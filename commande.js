799ec937a6f20f3ac3696c112f6d4ad405897e22
Christian Sanchez <csanch1@a-018818.ups.u-psud.fr>
2019-03-18 15:40:07 +0100

    Ajout des classes de commande

---
+++ commande.js
@@ -0,0 +1,39 @@
+class Commande
+{
+    constructor(nom, text, appareil, optionName, optionValue)
+    {
+        this.nom = nom;
+        this.text  = text;
+        this.appareils = [appareil];
+        this.action = new Action(optionName, optionValue);
+    }
+
+    ajoutAppareil(appareil)
+    {
+        if(appareil.constructor.name == appareils[0].constructor.name)
+        {
+            appareils.push(appareil);
+        }
+        else
+        {
+            throw new Error("L'appareil n'est pas du type " + appareils[0].constructor.name);
+        }
+    }
+
+    doAction()
+    {
+        for (const app of appareils) 
+        {
+            app.setOption(this.action.optionName, this.action.optionValue);   
+        }
+    }
+}
+
+class Action
+{
+    constructor(optionName, optionValue)
+    {
+        this.optionName = optionName;
+        this.optionValue = optionValue;
+    }
+}
