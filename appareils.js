4712709c902a479c0acd5944be5a81bda3192acc
Christian Sanchez <csanch1@a-018818.ups.u-psud.fr>
2019-03-18 15:03:45 +0100

    Ajouts des différents appareils

--- appareils.js
+++ appareils.js
@@ -47,7 +47,7 @@ class Porte extends Appareil
 
     getStatus()
     {
-        return this.ouvert ? "Ouvert" : "Fermé";
+        return this.ouvert ? "Ouverte" : "Fermée";
     }
 
     setOption(opName, option)
@@ -59,6 +59,143 @@ class Porte extends Appareil
         else if(opName == "Fermer")
         {
             this.ouvert = false;
+        }
+        else
+        {
+            throw new Error("L'option " + opName + " n'est pas définie");
+        }
+    }
+}
+
+class Fenetre extends Appareil
+{
+    constructor(nom)
+    {
+        super(nom, "Fenetre");
+        this.ouvert = false;
+        this.setNewOptions("Ouvrir", []);
+        this.setNewOptions("Fermer", []);
+    }
+
+    getStatus()
+    {
+        return this.ouvert ? "Ouverte" : "Fermée";
+    }
+
+    setOption(opName, option)
+    {
+        if(opName == "Ouvrir")
+        {
+            this.ouvert = true;
+        }
+        else if(opName == "Fermer")
+        {
+            this.ouvert = false;
+        }
+        else
+        {
+            throw new Error("L'option " + opName + " n'est pas définie");
+        }
+    }
+}
+
+
+class TV extends Appareil
+{
+    constructor(nom)
+    {
+        super(nom, "TV");
+        this.allumee = false;
+        this.chaine = "TV0";
+        this.volume = 0;
+        this.setNewOptions("Allumer", []);
+        this.setNewOptions("Eteindre", []);
+        this.setNewOptions("Chaine", ["HDMI", "AV", "PC", "TV0", "TV1", "TV2", "TV3", "TV4", "TV5", "TV6", "TV7", "TV8", "TV9", "TV10", "TV11", "TV12"]);
+        this.setNewOptions("Volume", ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]);
+    }
+
+    getStatus()
+    {
+        return this.allumee ? ("Allumée. Chaine : " + this.chaine + ". Volume : " + this.volume) : "Eteinte";
+    }
+
+    setOption(opName, option)
+    {
+        if(opName == "Allumer")
+        {
+            this.allumee = true;
+        }
+        else if(opName == "Eteindre")
+        {
+            this.allumee = false;
+        }
+        else if(opName == "Chaine")
+        {
+            if(this.options[opName].indexOf(option) != -1)
+            {
+                this.chaine = option;
+            }
+            else
+            {
+                throw new Error("La chaine " + option + " n'est pas reconnue");
+            }
+        }
+        else if(opName == "Volume")
+        {
+            if(this.options[opName].indexOf(option) != -1)
+            {
+                this.volume = parseInt(option);
+            }
+            else
+            {
+                throw new Error("Le volume " + option + " n'est pas défini");
+            }
+        }
+        else
+        {
+            throw new Error("L'option " + opName + " n'est pas définie");
+        }
+    }
+}
+
+
+class Chauffage extends Appareil
+{
+    constructor(nom)
+    {
+        super(nom, "Chauffage");
+        this.allume = false;
+        this.temperature = 0;
+        this.setNewOptions("Allumer", []);
+        this.setNewOptions("Eteindre", []);
+        this.setNewOptions("Temperature", ["15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"]);
+    }
+
+    getStatus()
+    {
+        return this.allume ? ("Allumé. Temperature : " + this.temperature + "°C") : "Eteint";
+    }
+
+    setOption(opName, option)
+    {
+        if(opName == "Allumer")
+        {
+            this.allume = true;
+        }
+        else if(opName == "Eteindre")
+        {
+            this.allume = false;
+        }
+        else if(opName == "Temperature")
+        {
+            if(this.options[opName].indexOf(option) != -1)
+            {
+                this.temperature = parseInt(option);
+            }
+            else
+            {
+                throw new Error("La température " + option + " n'est pas valide");
+            }
         }
         else
         {
